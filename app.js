37afc528b5914ae170039263afebce6e6046e671
Shruti <shrutijp@microland.com>
2021-08-08 19:55:32 +0530

    Weather app css and checks

--- app.js
+++ app.js
@@ -1,45 +1,91 @@
+require('dotenv').config();
 const express = require('express');
 const https = require("https");
 const bodyParser = require('body-parser');
-
-
+const ejs = require("ejs");
 
 const app = express();
+var cities = [];
+var error = "";
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
 
-    res.sendFile(__dirname + '/index.html');
+    res.render("index", { cityInfo: cities, error: error });
 
-})
+});
 
 app.post('/', function (req, res) {
-    const query = req.body.cityName;
-    const appid = "9a31fc9e6464859474028020360dedc";
+    var query = req.body.cityName;
+
+    if (query.includes(',')) {
+        query = query.split(',')[0];
+    }
+    let citiesName = cities.map(c => {
+        return c.name.toLowerCase();
+    });
+    
+    if (query) {
+        query = query[0].toUpperCase() + query.slice(1).toLowerCase();
+    }
+    
+    //const appid = "9a31fc9e6464859474028020360dedc";
+    const appid = process.env.APPID;
     const unit = "metric"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appid}1&units=${unit}`
     https.get(url, function (response) {
-        console.log(response.statusCode);
+        //console.log(response.statusCode);
         response.on("data", function (data) {
             const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const description = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-            res.write(`<p>The weather is ${description}</p>`);
-            res.write(`<h1>The temperature in ${query} is ${temp} degree celsius.</h1>`);
-            res.write(`<img src=${imageURL} alt="image"> `);
-            res.send();
+            // console.log(weatherData);
+            if (weatherData?.cod !== '404' && weatherData?.cod !== '400') {
+                const temp = weatherData.main.temp;
+                const description = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+                const country = weatherData?.sys?.country;
 
-
-    })
+                if (citiesName.includes(query.toLowerCase())) {
+                    error = `You already know the weather for ${query}.`;
+                }
+                else {
+                    const cityData = {
+                        name: query,
+                        image: imageURL,
+                        temperature: temp,
+                        description: description,
+                        country: country
     
- })
-    
-})
+                    }
+                    cities.push(cityData);
+                    error = "";
+                }
+            }
+            else {
+                error = "Enter a valid City!";
+                
+            }
+            res.redirect("/");
+            
 
+
+        },
+            response.on("error", function (e) {
+                console.log("error", e);
+            }),
+        )
+    
+    });
+    
+});
 
 
 app.listen(3000, function () {
     console.log("Server running");
-})
+});
+
+
+
